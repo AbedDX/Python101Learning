e0ed6cd10300c062dd53bfdcc510bfb58c56f12b
Abed <abbe_92@live.com>
2023-09-25 00:54:33 +0300

    demo

---
+++ 00_python_projects/fileSorter/app.py
@@ -0,0 +1,13 @@
+import os, shutil
+
+select_path = r"00_python_projects/fileSorter/"
+c = os.listdir(select_path)
+print(c)
+folder_name =   ["txt_file","image_file", "software_file","rar_file" ]
+for loop in range 
+x = os.path.exists(select_path + "txt")
+print(x)
+
+
+
+https://www.youtube.com/watch?v=gs0FNQR0njI&ab_channel=AlexTheAnalyst
