4bd69ebae7bb64a099da7def9e3e635f8be73eb7
Abed <abbe_92@live.com>
2023-09-26 18:26:26 +0300

    .

--- 00_python_projects/fileSorter/app.py
+++ 00_python_projects/fileSorter/app.py
@@ -1,13 +1,35 @@
-import os, shutil
+import customtkinter
 
-select_path = r"00_python_projects/fileSorter/"
-c = os.listdir(select_path)
-print(c)
-folder_name =   ["txt_file","image_file", "software_file","rar_file" ]
-for loop in range 
-x = os.path.exists(select_path + "txt")
-print(x)
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
+tk = customtkinter.CTk()
+tk.geometry("500x350")
 
+frame = customtkinter.CTkFrame(master=tk)
+frame.pack(pady=20,padx=60,fill="both", expand=True)
 
-https://www.youtube.com/watch?v=gs0FNQR0njI&ab_channel=AlexTheAnalyst
+#label = customtkinter.CTkLabel(master=frame, text =" FileSorting System")# add text font
+#label.pack(pady=12, padx=10)
+
+#entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Enter File Path")
+#entry1.pack(pady=12, padx=10)
+
+#button = customtkinter.CTkButton(master=frame, text="Sort")#command= "call the function"
+#button.pack(pady=12, padx=10)
+
+tk.mainloop()
+"""
+def inputPath():
+    path = input("Enter Path: ")
+    files = os.listdir(path)
+    for file in files:
+        filename,extension = os.path.splitext(file)
+        extension = extension[1:]
+
+        if os.path.exists(path+"/"+extension):
+            shutil.move(path+"/"+file,path+"/"+extension+"/"+file)
+        else:
+            os.makedirs(path+"/"+extension)
+            shutil.move(path+"/"+file,path+"/"+extension+"/"+file)
+"""
